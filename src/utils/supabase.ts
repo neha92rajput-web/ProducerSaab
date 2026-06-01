14bc9164661334ac3b6b488bfdeb2cd35aba3804
neha92rajput-web <neha92rajput@gmail.com>
2026-06-02 04:49:29 +0530

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://zxmzewdzytvecjgxzcde.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_9qZg4Lgzw7Q6M3Bs5_DgpQ_hQzzK7b8S3S2';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
