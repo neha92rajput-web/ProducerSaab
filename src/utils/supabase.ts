59c3047718bdeed15e678cc4ed2ddea3bb86bb87
neha92rajput-web <neha92rajput@gmail.com>
2026-06-02 15:37:28 +0530

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,3 +4,4 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://zxmzewdzytv
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4bXpld2R6eXR2ZWNqZ3h6Y2RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMjk2MzksImV4cCI6MjA5NTkwNTYzOX0.MdUOn6tgg8xEu8lokvY7wGiLkXHTbttxrXh6N3vJFbU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
